971c86addb1e19d1639c197ac991f78e506b134f
yhpgKIM <greenboy94@naver.com>
2021-04-15 17:40:23 +0900

    swe/1953/도둑잡기

--- Algo/src/com/algo/swe/swe_1953.java
+++ Algo/src/com/algo/swe/swe_1953.java
@@ -7,58 +7,67 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class swe_1953 {
-	static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
-	static int N, M, T, R, C, L;
+	static StringBuilder sb;
+	static int y, x, r, c, l;
 	static int[][] map;
 	static boolean[][] visited;
-	static int answer;
 	static Queue<int[]> q;
-	static int[] dy = { -1, 0, 1, 0 };
-	static int[] dx = { 0, 1, 0, -1 };
-	static boolean[][] status = { { true, true, true, true }, { true, false, true, false },
-			{ false, true, false, true }, { true, true, false, false }, { false, true, true, false },
-			{ false, false, true, true }, { true, false, false, true } };
+	static int[] dy = { 0, 0, 1, -1 };
+	static int[] dx = { 1, -1, 0, 0 };
+	static int answer;
+	static boolean[][] pipe = { { true, true, true, true }, { false, false, true, true }, { true, true, false, false },
+			{ true, false, false, true }, { true, false, true, false }, { false, true, true, false },
+			{ false, true, false, true } };
 
 	static void init() throws Exception {
-		st = new StringTokenizer(in.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		L = Integer.parseInt(st.nextToken());
-		answer = 0;
-		map = new int[N][M];
-		visited = new boolean[N][M];
-		q = new LinkedList<>();
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(in.readLine());
-			for (int j = 0; j < M; j++) {
+		st = new StringTokenizer(br.readLine());
+		y = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		c = Integer.parseInt(st.nextToken());
+		l = Integer.parseInt(st.nextToken());
+		map = new int[y][x];
+		visited = new boolean[y][x];
+
+		for (int i = 0; i < y; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < x; j++)
 				map[i][j] = Integer.parseInt(st.nextToken());
-			}
 		}
 
-		q.offer(new int[] { R, C });
+		answer = 0;
+
+	}
+
+	static void solve() {
+		bfs();
+		sb.append(answer + "\n");
 	}
 
 	static void bfs() {
-		visited[R][C] = true;
-		while (L-- > 0) {
+		q = new LinkedList<int[]>();
+		q.offer(new int[] { r, c });
+		visited[r][c] = true;
+
+		int[] cur;
+		int ny, nx, p, np;
+		while (l-- > 0) {
 			for (int size = 0, c = q.size(); size < c; size++) {
-				int[] cur = q.poll();
+				cur = q.poll();
 				answer++;
+				p = map[cur[0]][cur[1]];
 				for (int i = 0; i < 4; i++) {
-					int ny = cur[0] + dy[i];
-					int nx = cur[1] + dx[i];
-					int pipe = map[cur[0]][cur[1]];
-
-					if (nx < 0 || ny < 0 || nx >= M || ny >= N)
+					ny = cur[0] + dy[i];
+					nx = cur[1] + dx[i];
+					if (ny < 0 || ny >= y || nx < 0 || nx >= x)
 						continue;
-					if (!status[pipe - 1][i] || map[ny][nx] == 0 || visited[ny][nx])
+					if (!pipe[p - 1][i] || map[ny][nx] == 0 || visited[ny][nx])
 						continue;
-					int npipe = map[ny][nx];
-
-					if (!status[npipe - 1][(i + 2) % 4])
+					np = map[ny][nx];
+					if (!pipe[np - 1][i % 2 == 0 ? i + 1 : i - 1])
 						continue;
 					q.offer(new int[] { ny, nx });
 					visited[ny][nx] = true;
@@ -69,13 +78,14 @@ public class swe_1953 {
 
 	public static void main(String[] args) throws Exception {
 
-		T = Integer.parseInt(in.readLine());
-
-		for (int test = 1; test <= T; test++) {
+		int t = Integer.parseInt(br.readLine());
+		sb = new StringBuilder();
+		for (int test = 1; test <= t; test++) {
+			sb.append("#" + test + " ");
 			init();
-			bfs();
-			System.out.println("#" + test + " " + answer);
+			solve();
 		}
+		System.out.println(sb);
 	}
 
 }
