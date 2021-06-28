3b244d89df284a4ffa91c7764f8e0156e9b8c3cc
yhpgKIM <greenboy94@naver.com>
2021-06-28 21:04:16 +0900

    Algo

--- Algo/src/a/BOJ_1915.java
+++ Algo/src/a/BOJ_1915.java
@@ -17,12 +17,13 @@ public class BOJ_1915 {
 		st = new StringTokenizer(br.readLine());
 		n = Integer.parseInt(st.nextToken());
 		m = Integer.parseInt(st.nextToken());
-		map = new char[n][m];
+		map = new char[n + 1][m + 1];
+		dp = new int[n + 1][m + 1];
 
-		for (int i = 0; i < n; i++) {
+		for (int i = 1; i <= n; i++) {
 			String s = br.readLine();
-			for (int j = 0; j < m; j++) {
-				map[i][j] = s.charAt(j);
+			for (int j = 1; j <= m; j++) {
+				map[i][j] = s.charAt(j - 1);
 				if (map[i][j] == '1')
 					dp[i][j] = 1;
 			}
@@ -30,6 +31,16 @@ public class BOJ_1915 {
 	}
 
 	static void solve() {
+		int ans = 0;
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= m; j++) {
+				if (map[i][j] == '0')
+					continue;
+				dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+				ans = Math.max(dp[i][j], ans);
+			}
+		}
+		System.out.println(ans * ans);
 	}
 
 	public static void main(String[] args) throws Exception {
