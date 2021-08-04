34eb422ea8cc2676afcad56261cf4f9ead88a0f3
yhpgKIM <greenboy94@naver.com>
2021-08-04 22:41:00 +0900

    code: BOJ_15651

---
+++ Algo/src/BOJ/BOJ_15651.java
@@ -0,0 +1,47 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15651 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb;
+	static int n, m;
+	static boolean[] c;
+
+	static void init() throws Exception {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		c = new boolean[n + 1];
+
+		sb = new StringBuilder();
+	}
+
+	static void solve() {
+		for (int i = 1; i <= n; i++) {
+			dfs(1, String.valueOf(i));
+		}
+		System.out.println(sb);
+	}
+
+	static void dfs(int cnt, String s) {
+		if (cnt == m) {
+			sb.append(s + "\n");
+			return;
+		}
+		for (int i = 1; i <= n; i++) {
+			dfs(cnt + 1, s + " " + String.valueOf(i));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		init();
+		solve();
+	}
+
+}
