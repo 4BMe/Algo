49e1fb8df1f6bac1074798547e1febc6f25f63e0
yhpgKIM <greenboy94@naver.com>
2021-08-16 18:52:54 +0900

    code: BOJ_6987

--- Algo/src/a/BOJ_6987.java
+++ Algo/src/a/BOJ_6987.java
@@ -3,7 +3,6 @@ package a;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class BOJ_6987 {
@@ -11,76 +10,84 @@ public class BOJ_6987 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb;
-	static int[][] res;
-	static boolean[][] visited;
-	static boolean[] done;
-	static Map<String, Boolean> ch;
+	static int win[], lose[], draw[], t1[], t2[];
+	static HashMap<String, String> map;
+	static boolean avail;
 
 	static void init() throws Exception {
-		ch = new HashMap<>();
-		res = new int[6][3];
-		visited = new boolean[6][6];
-		done = new boolean[15];
-	}
+		int cnt = 0;
+		t1 = new int[15]; // 경기하는 2팀
+		t2 = new int[15];
+		for (int i = 0; i < 5; i++) {
+			for (int j = i + 1; j < 6; j++) {
+				t1[cnt] = i;
+				t2[cnt++] = j;
+			}
+		}
 
-	static void solve() throws Exception {
-		dfs(0);
 		sb = new StringBuilder();
 		for (int i = 0; i < 4; i++) {
 			String s = br.readLine();
-			sb.append(ch.containsKey(s) ? 1 : 0 + "\n");
+			st = new StringTokenizer(s);
+			win = new int[6];
+			lose = new int[6];
+			draw = new int[6];
+			avail = false;
+			int w = 0, l = 0, d = 0;
+			for (int j = 0; j < 6; j++) {
+				w += win[j] = Integer.parseInt(st.nextToken());
+				d += draw[j] = Integer.parseInt(st.nextToken());
+				l += lose[j] = Integer.parseInt(st.nextToken());
+			}
+			if (w + d + l != 30)
+				avail = false;
+			else
+				dfs(0);
+			if (avail)
+				sb.append(1 + " ");
+			else
+				sb.append(0 + " ");
 		}
 		System.out.println(sb);
 	}
 
 	static void dfs(int idx) {
+		if (avail)
+			return;
 		if (idx == 15) {
-			sb = new StringBuilder();
-			for (int i = 0; i < 6; i++) {
-				for (int j = 0; j < 3; j++) {
-					sb.append(res[i][j] + " ");
-				}
-			}
-			System.out.println(sb);
-			ch.put(sb.toString().trim(), true);
+			avail = true;
 			return;
 		}
-
-		for (int i = 0; i < 5; i++) {
-			for (int j = i + 1; j < 6; j++) {
-				if (visited[i][j])
-					continue;
-				visited[i][j] = true;
-				visited[j][i] = true;
-				// i 승
-				res[i][0]++;
-				res[j][2]++;
-				dfs(idx + 1);
-				res[i][0]--;
-				res[j][2]--;
-
-				// 무승
-				res[i][1]++;
-				res[j][1]++;
-				dfs(idx + 1);
-				res[i][1]--;
-				res[j][1]--;
-
-				// j 승
-				res[i][2]++;
-				res[j][0]++;
-				dfs(idx + 1);
-				res[i][2]--;
-				res[j][0]--;
-				visited[i][j] = false;
-				visited[j][i] = false;
-			}
+		int a = t1[idx];
+		int b = t2[idx];
+		// a가 이기는 경우
+		if (win[a] > 0 && lose[b] > 0) {
+			win[a]--;
+			lose[b]--;
+			dfs(idx + 1);
+			win[a]++;
+			lose[b]++;
+		}
+		// a와 b가 비기는 경우
+		if (draw[a] > 0 && draw[b] > 0) {
+			draw[a]--;
+			draw[b]--;
+			dfs(idx + 1);
+			draw[a]++;
+			draw[b]++;
+		}
+		// a가 지는 경우
+		if (lose[a] > 0 && win[b] > 0) {
+			lose[a]--;
+			win[b]--;
+			dfs(idx + 1);
+			lose[a]++;
+			win[b]++;
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
 		init();
-		solve();
 	}
 
 }
