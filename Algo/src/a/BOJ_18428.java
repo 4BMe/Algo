3643e175f376ae87905343eb2e08f39e0f7ee2a3
yhpgKIM <greenboy94@naver.com>
2021-08-21 23:36:52 +0900

    fix: BOJ_16987

---
+++ Algo/src/a/BOJ_18428.java
@@ -0,0 +1,25 @@
+package a;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_18428 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb;
+
+	static void init() throws Exception {
+	}
+
+	static void solve() {
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		init();
+		solve();
+	}
+
+}
