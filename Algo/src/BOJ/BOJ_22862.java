77a4a062fafb0ddca42bebc97d3b016817d0327e
yhpgKIM <greenboy94@naver.com>
2021-09-10 22:42:25 +0900

    code: BOJ_22862

---
+++ Algo/src/BOJ/BOJ_22862.java
@@ -0,0 +1,69 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_22862 {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb;
+	static int n, k;
+	static int[] num;
+
+	static void init() throws Exception {
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		k = Integer.parseInt(st.nextToken());
+		num = new int[n];
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < n; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+	}
+
+	static void solve() {
+		int ans = 0;
+
+		int start = 0;
+		int end = 0;
+		int odd = 0;
+		int even = 0;
+		if (num[start] % 2 == 0)
+			even = ans = 1;
+		else
+			odd++;
+
+		while (true) {
+			end++;
+			if (end == n)
+				break;
+			int now = num[end];
+			if (now % 2 == 0) {
+				even++;
+				ans = Math.max(ans, even);
+			} else {
+				odd++;
+				if (odd + 1 > k) {
+					if (num[start] % 2 == 0) {
+						even--;
+					} else {
+						odd--;
+					}
+					start++;
+				}
+			}
+		}
+
+		System.out.println(ans);
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		init();
+		solve();
+	}
+
+}
