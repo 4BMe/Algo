fee6ed29f9f3b03dd40ef1813f77702de1fabdfd
yhpgKIM <greenboy94@naver.com>
2021-11-26 18:24:37 +0900

    code: default file

---
+++ Algo/src/a/BOJ_.java
@@ -0,0 +1,28 @@
+package a;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_ {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+	static StringBuilder sb;
+
+	static void init() throws Exception {
+	}
+
+	static void solve() {
+	}
+
+	public static void main(String[] args) throws Exception {
+		long beforeTime = System.currentTimeMillis();
+		init();
+		solve();
+		long afterTime = System.currentTimeMillis();
+		long secDiffTime = (afterTime - beforeTime) / 1000;
+		System.out.println("시간차이(m) : " + secDiffTime);
+	}
+
+}
--- Algo/src/a/Programmers_.java
+++ Algo/src/a/Programmers_.java
@@ -4,25 +4,17 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class a {
+public class Programmers_ {
 
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer st;
 	static StringBuilder sb;
 
-	static void init() throws Exception {
-	}
-
 	static void solve() {
 	}
 
 	public static void main(String[] args) throws Exception {
-		long beforeTime = System.currentTimeMillis();
-		init();
 		solve();
-		long afterTime = System.currentTimeMillis();
-		long secDiffTime = (afterTime - beforeTime) / 1000;
-		System.out.println("시간차이(m) : " + secDiffTime);
 	}
 
 }
